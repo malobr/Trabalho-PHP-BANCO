98ac1b5727e7fcb2f1edf8567d95e328a12f5d2b
malobr <marcelotc1202@gmail.com>
2024-06-15 17:49:41 -0300

    Nova pasta com o trabalho adiciodadas, Tela de login adicionado

---
+++ Trabalho/TelaDeLogin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div class="box">
+        <span class="borderLine"></span>
+        <form>
+            <h2>Cyber Login</h2>
+            <div class="inputBox">
+                <input type="text"  required="required" name="" id="">
+                <span>Usuario</span>
+                <i></i>
+            </div>
+            <div class="inputBox">
+                <input type="password"  required="required" name="" id="">
+                <span>Senha</span>
+                <i></i>
+            </div>
+            <div class="link">
+                <a href="Cadastro.php">Cadastrar</a>
+            </div>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
