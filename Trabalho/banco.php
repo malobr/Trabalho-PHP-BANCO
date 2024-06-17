ac684fd92e67bd42e432511d9204b794292559c4
hirowatanabe01 <hiroshi.watanabe@cs.up.edu.br>
2024-06-17 14:04:49 -0300

    Functions to deal with users.

--- Trabalho/banco.php
+++ Trabalho/banco.php
@@ -16,4 +16,74 @@ function createOnDB($into, $values)
     echo "<br> Query: $q";
     echo var_dump($resp);
 }
+
+function criarUsuario(string $usuario, string $nome, string $senha, string $tipo = 'visualizador', $debug = false): void
+{
+    global $banco;
+
+    $senha = password_hash($senha, PASSWORD_DEFAULT);
+
+    $q = "INSERT INTO usuarios(cod, usuario, nome, senha, tipo) VALUES (NULL, '$usuario', '$nome', '$senha', '$tipo')";
+
+    $resp = $banco->query($q);
+
+    if ($debug) {
+        echo "<br> Query: $q";
+        echo var_dump($resp);
+    }
+}
+
+function deletarUsuario(string $usuario): void
+{
+    global $banco;
+
+    $q = "DELETE FROM usuarios WHERE usuario='$usuario'";
+
+    $resp = $banco->query($q);
+    echo "<br> Query: $q";
+    echo var_dump($resp);
+}
+
+function atualizarUsuario(string $usuario, string $nome = "", string $senha = "", string $tipo = "", bool $debug = false): void
+{
+    global $banco;
+
+    $set = [];
+    if ($nome != "") {
+        $set[] = "nome='$nome'";
+    }
+    if ($senha != "") {
+        $novaSenha = password_hash($senha, PASSWORD_DEFAULT);
+        $set[] = "senha='$novaSenha'";
+    }
+    if ($tipo != "") {
+        $set[] = "tipo='$tipo'";
+    }
+
+    $set = implode(', ', $set);
+
+    $q = "UPDATE usuarios SET $set WHERE usuario='$usuario'";
+
+    $resp = $banco->query($q);
+
+    if ($debug) {
+        echo "<br> Query: $q";
+        echo var_dump($resp);
+    }
+}
+
+function buscarTipoUsuario(string $usuario)
+{
+    global $banco;
+
+    $q = "SELECT tipo FROM usuarios WHERE usuario='$usuario'";
+    $resultado = $banco->query($q);
+
+    if ($resultado && $resultado->num_rows > 0) {
+        $tipo = $resultado->fetch_assoc()['tipo'];
+        return $tipo;
+    } else {
+        return null;
+    }
+}
 ?>
