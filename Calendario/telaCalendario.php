c65fb614ef34ca6966976af40392a340b5266d98
Brian Snack <122538036+briansnack@users.noreply.github.com>
2024-06-17 23:58:11 -0300

    Aplicando CSS na telaCalendário

--- Calendario/telaCalendario.php
+++ Calendario/telaCalendario.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendário PHP</title>
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <div class="principal">
