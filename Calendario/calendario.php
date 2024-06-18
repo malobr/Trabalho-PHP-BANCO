de1c9836eb24707bbc0bc4bc2667f388ddf4871e
Brian Snack <122538036+briansnack@users.noreply.github.com>
2024-06-17 23:57:30 -0300

    Aplicando CSS no calendário

--- Calendario/calendario.php
+++ Calendario/calendario.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendário</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
 <?php
 function construcao_calendario($mes, $ano) {
     $diasDaSemana = array('Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb');
@@ -62,3 +71,5 @@ if (isset($_GET['mes']) && isset($_GET['ano'])) {
 
 construcao_calendario($mes, $ano);
 ?>
+</body>
+</html>
